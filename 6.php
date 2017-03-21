11bb5f40e4444152cc476cd9f59986293e7a53ad
Shkodenko V. Taras <podlom@users.noreply.github.com>
2017-03-21 11:49:16 +0200

    Update 6.php
    Можно так сократить инициализацию

--- 6.php
+++ 6.php
@@ -1,7 +1,6 @@
 <?php
 $arr = array('green'=>'зеленый', 'red'=>'красный','blue'=>'голубой');
-$en = [];
-$ru = [];
+$en = $ru = [];
 foreach ($arr as $key => $value){
     $en[] = $key;
     $ru[] = $value;
